e9a14c253eaf399e7fd74dc5691b614b7c725c53
G. Braz <gestao@vivaeventos.com.br>
2025-12-04 12:09:17 -0300

    refactor: aprimorar configuração do Next.js, desabilitando source maps em produção e ajustando suporte a path aliases

--- vendas-refatorado/next.config.js
+++ vendas-refatorado/next.config.js
@@ -1,18 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // Desabilitar source maps em produção para segurança
+  productionBrowserSourceMaps: false,
+  
   images: {
     domains: [],
     unoptimized: true,
   },
+  
   // Suporte a path aliases
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': require('path').resolve(__dirname, 'src'),
     };
+    
+    // Em produção, desabilitar source maps completamente
+    if (!dev) {
+      config.devtool = false;
+    }
+    
     return config;
   },
+  
+  // Desabilitar headers que expõem informações
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
